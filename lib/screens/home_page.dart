b55be67b538b4ba615f629aecc6d9115f65f13cb
jesusv0107 <jesus.velasco@rdpolytech.ca>
2024-08-28 20:39:22 -0600

    Push

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,20 +10,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // Reference to Firestore database
   FirebaseFirestore db = FirebaseFirestore.instance;
 
-  // List of tasks
   final List<String> tasks = <String>[];
-  // List of checkboxes states corresponding to tasks
+  //List of checkbosex states corresponding to tasks
   final List<bool> checkboxes = List.generate(8, (index) => false);
-  // Controller for the text input
+  //controller for the text input
   TextEditingController nameController = TextEditingController();
 
-  // Boolean to manage checkbox state
   bool isChecked = false;
 
-  // Function to add a new task to the list and Firestore
   void addItemToList() async {
     final String taskName = nameController.text;
 
@@ -39,12 +35,11 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  // Function to remove a task from the list and Firestore
   void removeItems(int index) async {
-    // Get the task to be removed
+    //get the tasks to be removed
     String taskToBeRemoved = tasks[index];
 
-    // Remove the task from Firestore
+    //Remove the task from Firestore
     QuerySnapshot querySnapshot = await db
         .collection('tasks')
         .where('name', isEqualTo: taskToBeRemoved)
@@ -65,180 +60,173 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // App bar with a logo and title
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 255, 0, 0),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              height: 80,
-              child: Image.asset('assets/rdplogo.png'), // Logo image
-            ),
-            const Text(
-              'Daily Planner',
-              style: TextStyle(
-                  fontFamily: 'Caveat', fontSize: 32, color: Colors.white),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 0, 66, 56),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 80,
+                child: Image.asset('assets/rdplogo.png'),
+              ),
+              Text(
+                'Daily Planner',
+                style: TextStyle(
+                    fontFamily: 'Caveat',
+                    fontSize: 32,
+                    color: Color.fromARGB(255, 115, 255, 0)),
+              ),
+            ],
+          ),
         ),
-      ),
-      body: Stack(
-        children: [
-          // Background image
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                    'assets/background_image.jpg'), // Background image
-                fit: BoxFit.cover,
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/rdplogonew.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          // White background container for calendar and tasks
-          Container(
-            color:
-                Colors.white.withOpacity(0.8), // White background with opacity
-            child: Column(
-              children: [
-                // Calendar widget
-                TableCalendar(
-                  calendarFormat: CalendarFormat.month,
-                  headerVisible: true,
-                  focusedDay: DateTime.now(),
-                  firstDay: DateTime(2023),
-                  lastDay: DateTime(2025),
-                ),
-                // Task list widget
-                Container(
-                  height: 300,
-                  child: Expanded(
+            Container(
+              child: Column(
+                children: [
+                  TableCalendar(
+                    calendarFormat: CalendarFormat.month,
+                    headerVisible: true,
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2023),
+                    lastDay: DateTime(2025),
+                  ),
+                  Container(
+                    height: 100,
                     child: ListView.builder(
                       shrinkWrap: true,
                       itemCount: tasks.length,
                       itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          margin: const EdgeInsets.only(top: 3.0),
-                          decoration: BoxDecoration(
-                            color: checkboxes[index]
-                                ? const Color.fromARGB(255, 255, 255, 255)
-                                    .withOpacity(0.7)
-                                : Color.fromARGB(255, 255, 0, 0)
-                                    .withOpacity(0.7),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Row(
-                              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Icon(
-                                  size: 44,
-                                  !checkboxes[index]
-                                      ? Icons.manage_history
-                                      : Icons.playlist_add_check_circle,
-                                ),
-                                SizedBox(width: 18),
-                                Expanded(
-                                  child: Text(
-                                    '${tasks[index]}',
-                                    style: checkboxes[index]
-                                        ? TextStyle(
-                                            decoration:
-                                                TextDecoration.lineThrough,
-                                            fontSize: 20,
-                                            color:
-                                                Colors.black.withOpacity(0.5),
-                                          )
-                                        : TextStyle(fontSize: 25),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                ),
-                                Row(
+                        return SingleChildScrollView(
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 2),
+                            decoration: BoxDecoration(
+                              color: checkboxes[index]
+                                  ? Color.fromARGB(255, 170, 219, 30)
+                                  : Color.fromARGB(255, 170, 219, 30),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Container(
+                                child: Row(
+                                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Transform.scale(
-                                      scale: 1.2,
-                                      child: Checkbox(
-                                        value: checkboxes[index],
-                                        onChanged: (newValue) {
-                                          setState(() {
-                                            checkboxes[index] = newValue!;
-                                          });
-                                          // To-Do: updateTaskCompletionStatus()
-                                        },
+                                    Icon(
+                                      size: 44,
+                                      !checkboxes[index]
+                                          ? Icons.manage_history
+                                          : Icons.playlist_add_check_circle,
+                                    ),
+                                    SizedBox(width: 18),
+                                    Expanded(
+                                      child: Text(
+                                        '${tasks[index]}',
+                                        style: checkboxes[index]
+                                            ? TextStyle(
+                                                decoration:
+                                                    TextDecoration.lineThrough,
+                                                fontSize: 20,
+                                                color: Colors.black
+                                                    .withOpacity(0.5),
+                                              )
+                                            : TextStyle(fontSize: 25),
                                       ),
                                     ),
-                                    IconButton(
-                                      color: Color.fromARGB(255, 0, 0, 0),
-                                      hoverColor:
-                                          Color.fromARGB(255, 0, 255, 0),
-                                      iconSize: 30,
-                                      icon: Icon(Icons.delete),
-                                      onPressed: () {
-                                        removeItems(
-                                            index); // Remove task on press
-                                      },
+                                    Row(
+                                      children: [
+                                        Transform.scale(
+                                          scale: 1.5,
+                                          child: Checkbox(
+                                              value: checkboxes[index],
+                                              onChanged: (newValue) {
+                                                setState(() {
+                                                  checkboxes[index] = newValue!;
+                                                });
+                                                //To-Do: updateTaskCompletionStatus()
+                                              }),
+                                        ),
+                                        IconButton(
+                                          color: Colors.black,
+                                          hoverColor:
+                                              Color.fromARGB(175, 0, 66, 56),
+                                          iconSize: 30,
+                                          icon: Icon(Icons.delete),
+                                          onPressed: () {
+                                            removeItems(index);
+                                          },
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
-                              ],
+                              ),
                             ),
                           ),
                         );
                       },
                     ),
                   ),
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: nameController,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(21),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          labelText: 'Add To Do List Item',
-                          labelStyle: TextStyle(
-                            fontSize: 26,
-                            color: Color.fromARGB(255, 255, 0, 0),
-                          ),
-                          hintText: 'Enter your task here', //inputDecoration
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          child: TextField(
+                            controller: nameController,
+                            maxLength: 90,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(23),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              labelText: 'Add To-Do List Item',
+                              labelStyle: TextStyle(
+                                fontSize: 26,
+                                color: const Color.fromARGB(255, 0, 66, 56),
+                              ),
+                              hintText:
+                                  'Enter your task here', //inputdecoration
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    //to-do clearTextField
-                    const IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: null,
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(3.0),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.red),
-                    ),
-                    child: Text(
-                      'Add To-Do List Item',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      addItemToList();
-                    },
+                      const IconButton(
+                        onPressed: null,
+                        icon: Icon(Icons.clear),
+                      ),
+                    ],
                   ),
-                )
-              ],
+                  Padding(
+                    padding: EdgeInsets.all(.005),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        addItemToList();
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                            Color.fromARGB(255, 171, 218, 0)),
+                      ),
+                      child: Text(
+                        'Add To-Do List Item',
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 0, 119, 101)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
